4d0baa7f6e9790fb939557be9f15d74d16ce2212
aswinrajkallil <aswinrajcholakuzhi@gmail.com>
2026-05-28 23:04:06 +0530

    Add @vercel/analytics dependency and integrate Analytics component in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import React from 'react'
 import Weather from './components/Weather'
 import './components/Weather.css'
 import Footer from './components/Footer'
+import { Analytics } from "@vercel/analytics/react"
 
 
 const App = () => {
   return (
     <div className='app'>
       <h1 className="weather-title">Weather App</h1>
+
       <Weather />
       <Footer />
+
+    <Analytics />
     </div>
   )
 }
